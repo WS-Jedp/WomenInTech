b039e68987cad485f7013a9f8e501004207f3d46
Jedp <jedp082@gmail.com>
2020-06-30 16:34:51 -0500

    feature/webpack - All webpack and architecture of the project done

---
+++ src/Index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+
+// Styles
+import './assets/styles/main.css';
+
+// Routes
+import App from './routes/App';
+
+reactDOM.render(<App />, document.getElementById('app'));
