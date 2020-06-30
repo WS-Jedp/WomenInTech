d8a4ab22bce871b9c9bad57e37fe98feadd98341
Jedp <jedp082@gmail.com>
2020-06-30 16:54:27 -0500

    feature/styles - Main Styles create and reset styles done

---
+++ src/layouts/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Home extends React.Component{
+  constructor(){
+    super();
+  }
+
+  render(){
+    return(
+      <>
+        <h1>Hello World From Home!</h1>
+      </>
+    );
+  }
+}
+
+export default Home;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { BrowserRouter,Route, Switch } from 'react-router-dom';
 
+// Layouts
+import Home from '../layouts/Home/Home';
+import Detail from '../layouts/Detail/Detail';
+
 const App = () => {
   return(
     <BrowserRouter>
       <Switch>
-        <Route exact path='/' component='Hello World'></Route>
+        <Route exact path='/' component={Home}></Route>
+        <Route exact path='/detail' component={Detail}></Route>
       </Switch>
     </BrowserRouter>
   );
