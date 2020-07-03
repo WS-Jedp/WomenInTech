96fe237db58390f3c214951fbef1d48ddbe16a51
Juan Jose Pelaez <juanhernandez.code@gmail.com>
2020-07-03 17:50:54 -0500

    Se agreagan miniaturas en el detalle y se hace mas grande la parte del contenido

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import CardSmall from '../../components/CardSmall/CardSmall';
+import { Link } from 'react-router-dom';
 
 // Styles
 import './DetailStyles.css';
@@ -7,17 +9,29 @@ import './DetailStyles.css';
 // Components
 import CardDetail from '../../components/CardDetail/CardDetail';
 
-function Detail(){
-    // let { name } = useParams();
-    return(
-      <section className="padding-big detail">
-        <div>
+function Detail() {
+  // let { name } = useParams();
+  return (
+    <section className="padding-big detail">
+      <div className="detail__header">
+        <div className="detail__header-title">
+          <Link to="/dates">
+            <img src="../../src/assets/Images/left_arrow.svg" alt="" srcset="" />
+          </Link>
           <h2 className="text-color-main">1800 - Algorithms</h2>
-          <hr className="bg-color-main" />
-          <CardDetail />
         </div>
-      </section>
-    );
+        <hr className="bg-color-main" />
+      </div>
+
+      <CardDetail />
+      <div className="detail__thumbnails">
+        <CardSmall />
+        <CardSmall />
+        <CardSmall />
+      </div>
+
+    </section>
+  );
 }
 
 export default Detail;
