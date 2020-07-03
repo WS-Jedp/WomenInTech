0c3b2a294213c59b07f9b4316b26592442fe88f7
Juan Jose Pelaez <juanhernandez.code@gmail.com>
2020-07-03 09:14:52 -0500

    Add image title and button

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // Styles
 import './WelcomeStyles.css';
 
-class Welcome extends React.Component{
-  constructor(){
+class Welcome extends React.Component {
+  constructor() {
     super()
   }
 
-  render(){
+  render() {
 
     return (
-      <section>
-        Welcome Page(°- ° )
-      </section>
+      <Link to='/dates'>
+        <section className="welcome__container" style={{ backgroundImage: "url('../../src/assets/Images/background_welcome.png')" }}>
+          <h1 className="welcome__tittle text-color-main padding-normal">Honoring women in tech</h1>
+          <img className="welcome__image" src="../../src/assets/Images/arms.svg" alt="Honoring women in tech" />
+          <button className="margin-normal button-primary">Know More</button>
+        </section>
+      </Link >
     );
   }
 }
